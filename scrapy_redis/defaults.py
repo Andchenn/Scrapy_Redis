46220b74524cc517479614c0fc3458127f71102f
Andchen <1476204794@qq.com>
2018-09-10 12:14:38 +0800

    defualts values

---
+++ scrapy_redis/defaults.py
@@ -0,0 +1,21 @@
+# 供独立使用
+DUPEFILTER_KEY = 'dupefilter:%(timestamp)s'
+
+PIPELINE_KEY = '%(spider)s:items'
+RIDIS_CLS = 'redis.StrictRedis'
+REDIS_ENCODING = 'utf-8'
+# Sane连接默认值
+REDIS_PARAMS = {
+    'socket_timeout': 30,
+    'socket_connect_timeout': 30,
+    'retry_on_timeout': True,
+    'encoding': REDIS_ENCODING,
+}
+
+SCHEDULER_QUEUE_KEY = '%(spider)s:requests'
+SCHEDULER_QUEUE_CLASS = 'scrapy_redis.queue.PriorityQueue'
+SCHEDULER_DUPEFILTER_KEY = '%(spider)s:dupefilter'
+SCHEDULER_DUPEFILTER_CLASS = 'scrapy_redis.dupefilter.RFPDupeFilter'
+
+START_URLS_KEY = '%(name)s:start_urls'
+START_URLS_AS_SET = False
