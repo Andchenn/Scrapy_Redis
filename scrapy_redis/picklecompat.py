5453d73a2ad617a5a2e46b4961c111ab7992c0f9
Andchen <1476204794@qq.com>
2018-09-10 10:49:41 +0800

    实现了loads和dumps两个函数

---
+++ scrapy_redis/picklecompat.py
@@ -0,0 +1,13 @@
+"""A pickle wrapper module with protocol=-1 by default"""
+try:
+    import cPickle as pickle
+except ImportError:
+    import pickle
+
+
+def loads(s):
+    return pickle.loads(s)
+
+
+def dumps(obj):
+    return pickle.dumps(obj, protocol=-1)
